bc07d911558b16770d4d88a09195ee37041dba61
vivz <vivianzhang61@gmail.com>
2017-04-29 16:27:16 -0700

    If applied, this commit will allow you to run an automated grammar generation and check process. Refer to the comments in each file for instruction

--- tools/SingleGrammarGenerator.py
+++ tools/SingleGrammarGenerator.py
@@ -1,16 +1,21 @@
+#
+#Description: The SingleGrammarGenerator class randomly generate a grammar based on
+#   the given list of terminals and non-terminals 
+#
 #Input: nonTerminals: a list of characters representing the nonterminals of length 2-4
-#       Terminals: a list of characters representing the terminals of length 2-4
-
+#   Terminals: a list of characters representing the terminals of length 2-4
+#
 #Output: a grammar represented by a dictionary of rules, where the keys are the 
-# nonterminal symbols and the corresponding values are a list of productions. For example:
-#       grammar = {
-#           'S': ['F', '(S+F)'],
-#           'F': ['a']}
-#where 'S' and 'F' are nonterminal symbols, 'a','(','+' and ')' are terminal symbols
-
+#   nonterminal symbols and the corresponding values are a list of productions. For example:
+#   grammar = {
+#       'S': ['F', '(S+F)'],
+#       'F': ['a']}
+#   where 'S' and 'F' are nonterminal symbols, 'a','(','+' and ')' are terminal symbols
+#
 #Usage:
-# g = SingleGrammarGenerator(['S','F'],['a','(','+',')'])
-# g.generate()
+#   g = SingleGrammarGenerator()
+#   g.generate(['S','F'],['a','(','+',')'])
+#
 
 import random
 from random import randint
@@ -19,7 +24,8 @@ epsilon = 'ε'
     
 class SingleGrammarGenerator:
     
-    def __init__(self, nonTerminals, terminals):
+    def generate(self, nonTerminals, terminals):
+        #TODO: Make the randomization weighted to maximize the number of valid grammars generated 
         if not (isinstance(nonTerminals,list) and isinstance(terminals,list)):
             raise Exception('Input terminals/nonterminals need to be in format of lists ')
         if len(nonTerminals)>4 or len(nonTerminals)<2 or len(terminals)>4 or len(terminals)<2:
@@ -32,26 +38,34 @@ class SingleGrammarGenerator:
         
         for v in self.nonTerminals:
             self.grammar[v]=[]
-    
-    def generate(self):
+
         for v in self.nonTerminals:
             p = ""
+            #The number of productions associated with a non-terminal is chosen at random
             r = randint(1,3)
             i = 0
-            while i < r:
-                p = self.production()
-                if not p in self.grammar[v]:
-                    self.grammar[v].append(p)
-                    i+=1
-                if r==1 and epsilon in self.grammar[v]:
-                    i-=1
-        print (self.grammar)
-        
-    def production(self):
-        r = randint(0,4)
-        p = ""
+            if r==1:
+                #If there is only one production, this symbol is not nullible
+                self.grammar[v].append(self.production(False))
+            else:
+                while i < r:
+                    p = self.production()
+                    if not p in self.grammar[v]:
+                        self.grammar[v].append(p)
+                        i+=1
+        return self.grammar
+
+    def production(self,nullible=True):
+        #Randomly generate a production of a random length
+        #All terminals and non-terminals have equal chance of being chosen
+        #If nullible is set to false, then the production cannot be an epsilon
+        if nullible:
+            r = randint(0,4)
+        else:
+            r = randint(1,4)
         if r==0:
             return epsilon
+        p = ""
         for i in range(0,r):
             p+=random.choice(self.symbols)
         return p
