9d55a1f3171a397a29c21f4b5a4b38e4fec5900e
jbrundrett <julien.brundret@gmail.com>
2017-05-04 17:31:33 -0700

    Implement client side answer validations

--- static/learn.js
+++ static/learn.js
@@ -27,31 +27,42 @@ function query_for_question() {
 
 function draw_question() {
 	$('#questions-container').append(question_template(question_data));
-	
+
 	// bind form submit handler: check if answer is correct
 	$('#question-input').on('submit', function() {
 		
+		// validate input before moving into the ajax request
 		var input_value = $('#question-answer').val();
+		var input_trimmed = input_value.replace(/\s/g,'')
+		var valid = input_trimmed.match('^([a-z$],)*[a-z$],?$') != null;
+		// console.log(valid)
 
-		$.ajax({
-			type: "POST",
-			url: "/check_answer",
-			data : { 
-				// 'question_data': question_data,
-				'category': question_data.category,
-				'symbol': question_data.symbol,
-				'answer': $('#question-answer').val()
-			},
-			success: function(results) {
-				console.log(results)
-				if (results.correct) {
-					$('#question-input').remove()
-					query_for_question()
+		if (valid) {
+			$.ajax({
+				type: "POST",
+				url: "/check_answer",
+				data : { 
+					// 'question_data': question_data,
+					'category': question_data.category,
+					'symbol': question_data.symbol,
+					'answer': $('#question-answer').val()
+				},
+				success: function(results) {
+					console.log(results)
+					if (results.correct) {
+						$('#question-input').remove()
+
+						query_for_question()
+					} else {
+
+					}
+				},
+				error: function(error) {
+					console.log(error)
 				}
-			},
-			error: function(error) {
-				console.log(error)
-			}
-		});
+			});
+		} else {
+
+		}
 	});
 }
