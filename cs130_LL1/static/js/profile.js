0673c51f67c7770c1264d80266a32dba7986d1c2
vivz <vivianzhang61@gmail.com>
2017-05-21 02:25:33 -0700

    two seperate tables, also picked the color

--- cs130_LL1/static/js/profile.js
+++ cs130_LL1/static/js/profile.js
@@ -4,19 +4,55 @@ $(document).ready(function() {
   create_chart()
 })
 
-var chart = c3.generate({
+var gD = [
+            ['data1', 30],
+            ['data2', 120],
+        ];
+  
+gD[0][0] = "Skipped: " + gD[0][1];
+gD[1][0] = "Completed: " + gD[1][1];
+
+var grammarChart = c3.generate({
+    bindto: '#grammar_chart',
+    color: {
+        pattern: ['#DBE6EA','#5AB2D1',]
+    },
     data: {
-        columns: [
-            ['Incorrect Answers', 30],
-            ['Correct Answers', 30],
-            ['Skipped Grammars', 120],
-        ],
+        columns: gD,
+        type : 'donut',
+        // onclick: function (d, i) { console.log("onclick", d, i); },
+        // onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        // onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    donut: {
+        title: "Grammars Learned"
+    }
+});
+
+var qD = [
+            ['data1', 2],
+            ['data2', 40],
+        ];
+
+qD[0][0] = "Wrong: " + qD[0][1];
+qD[1][0] = "Correct: " + qD[1][1];
+
+
+var questionChart = c3.generate({
+    bindto: '#question_chart',
+    color: {
+        pattern: ['#F7C996', '#F6781D']
+    },
+    data: {
+        columns: qD,
         type : 'donut',
         onclick: function (d, i) { console.log("onclick", d, i); },
         onmouseover: function (d, i) { console.log("onmouseover", d, i); },
         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     donut: {
-        title: "Grammars Learned"
+        title: "Questions Answered"
     }
 });
+
+
