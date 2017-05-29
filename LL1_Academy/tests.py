a1e65ad9bd80f77e4d9ec435954d277dbf4ebead
Howard Huang <howardhuang96@gmail.com>
2017-05-28 22:05:49 -0700

    add more simple test cases for correct template rendering

--- LL1_Academy/tests.py
+++ LL1_Academy/tests.py
@@ -3,11 +3,16 @@ from django.urls import reverse
 from LL1_Academy.models import Grammar, Question
 
 from django.contrib.sites.models import Site
+from django.contrib.auth.models import User
 
 class TestData(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        user = User.objects.create_user(username='test',
+                                        email='test@gmail.com',
+                                        password='test')
+
         g = Grammar(prods="{'A': ['xA', 'Bz'],'B': ['yB']}", nonTerminals="AB", terminals="xyz", startSymbol="A")
         g.save()
         q = Question(gid=g, qnum=0, category="FI", symbol="A", answer="xy")
@@ -30,8 +35,7 @@ class TestData(TestCase):
         )
 
 
-class UrlTest(TestData):
-
+class RoutingTest(TestData):
     def test_index1(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -61,7 +65,26 @@ class RenderingTest(TestData):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'LL1_Academy/index.html')
     
-    def test_index1(self):
+    def test_index2(self):
+        response = self.client.get('/index')
+        self.assertTemplateUsed(response, 'LL1_Academy/index.html')
+    
+    def test_about(self):
+        response = self.client.get('/about')
+        self.assertTemplateUsed(response, 'LL1_Academy/about.html')
+    
+    def test_profile(self):
+        c = Client()
+        c.login(username='test', password='test')
+        response = c.get('/profile')
+        self.assertTemplateUsed(response, 'LL1_Academy/profile.html')
+    
+    def test_learn(self):
         response = self.client.get('/learn')
         self.assertTemplateUsed(response, 'LL1_Academy/learn.html')
 
+    def test_error_page(self):
+        response = self.client.get('/get_404_page')
+        self.assertTemplateUsed(response, 'LL1_Academy/error.html')
+
+# class GrammarTest(TestData):
