2e136723f4bc28df1d72455c241982dd15df8004
jbrundrett <julien.brundret@gmail.com>
2017-06-01 23:14:06 -0700

    moving files around, slight fixes and such

---
+++ cs130_LL1/static/js/tutorial-questions.js
@@ -0,0 +1,127 @@
+var q1_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['xy']
+    }]
+}
+
+
+var q2_grammar = {
+	grammar: [
+    {
+		nt: 'A',
+		productions: ['xy','yz']
+	}]
+}
+
+var q3_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['Bx', 'z']
+    },
+    {
+		nt: 'B',
+		productions: ['y']
+	}]
+}
+
+var q4_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['BC', 'zC']
+    },
+    {
+		nt: 'B',
+		productions: ['Cx','yBy']
+	},
+	{
+		nt: 'C',
+		productions: ['xz']
+	}]
+}
+
+var firstQuestions = [
+	{
+		grammar: q1_grammar,
+		helptext: "To calculate the First Set of A, we need to calculate the First Set for the production “xy”. For this case, we just find first terminal symbol of the production. What is the first terminal symbol of “xy”?",
+		question: "What is the first set of A?",
+		answer: "x",
+		type: "checkbox",
+		terminals: ['x','y'],
+	},
+	{
+		grammar: q2_grammar,
+		helptext: "When there are multiple productions, we calculate the First Set of each production individually, and add each of them to the First Set. The First Set of A are the First Sets of “xy” and “yz” combined.",
+		question: "What is the first set of A?",
+		answer: "x,y",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: q3_grammar,
+		helptext: "When there is a nonterminal as the first symbol of a production, then the First Set of that symbol is the First Set of that production. In this case, the First Set of A is “z”, and the First Set of B. Hint: It would be good to calculate First Set of B first, and then substitute it into the First Set of A.",
+		question: "What is the first set of B?",
+		answer: "y",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: q3_grammar,
+		helptext: "When there is a nonterminal as the first symbol of a production, then the First Set of that symbol is the First Set of that production. In this case, the First Set of A is “z”, and the First Set of B. Hint: It would be good to calculate First Set of B first, and then substitute it into the First Set of A.",
+		question: "What is the first set of A?",
+		answer: "y,z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: q4_grammar,
+		helptext: "This is a challenge problem",
+		question: "What is the first set of A?",
+		answer: "x,y,z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: q4_grammar,
+		helptext: "This is a challenge problem",
+		question: "What is the first set of B?",
+		answer: "x,z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: q4_grammar,
+		helptext: "This is a challenge problem",
+		question: "What is the first set of C?",
+		answer: "x",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	}
+]
+
+var followQuestions = [
+	// filler code
+	{
+		grammar: q3_grammar,
+		helptext: "THIS IS FILLER CODE FOR FOLLOW QUESTIONS",
+		question: "THIS IS FILLER CODE FOR FOLLOW QUESTIONS",
+		answer: "y,z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	}
+]
+
+var parseQuestions = [
+	// filler code
+	{
+		grammar: q3_grammar,
+		helptext: "THIS IS FILLER CODE FOR PARSE TABLE QUESTIONS",
+		question: "THIS IS FILLER CODE FOR PARSE TABLE QUESTIONS",
+		answer: "y",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	}
+]
---
+++ cs130_LL1/static/js/tutorial.js
@@ -0,0 +1,148 @@
+var grammar_template_src   = $("#grammar-template").html();
+var grammar_template = Handlebars.compile(grammar_template_src);
+var question_template_src   = $("#question-template").html();
+var question_template = Handlebars.compile(question_template_src);
+
+var questions;
+var currentQ;
+
+var currentSection; // "first", "follow", "parse"
+
+$(document).ready(function() {
+	// switchSection("first");
+})
+
+function switchSection(section) {
+	if (section == currentSection)
+		return;
+
+	switch(section) {
+		case "first":
+			questions = firstQuestions;
+			break;
+		case "follow":
+			questions = followQuestions;
+			break;
+		case "parse":
+			questions = parseQuestions;
+			break;
+		default:
+			console.log("invalid section, try again");
+			return;
+	}
+
+	if (currentSection) { // if statement needed for first page load case
+		$("#" + currentSection + "Tutorial").removeClass("active");
+	} else { // on first page laod
+		$("#initialExplainer").hide();
+		$("#grammar-container").fadeIn();
+		$("#questions-wrapper").fadeIn();
+	}
+	currentSection = section;
+	$("#" + currentSection + "Tutorial").addClass("active");
+	currentQ = 0;
+	load_next_question();
+}
+
+$('#firstTutorial').click(function() {
+	switchSection("first");
+});
+$('#followTutorial').click(function() {
+	switchSection("follow");
+});
+$('#parseTutorial').click(function() {
+	switchSection("parse");
+});
+
+$('.button.prevQ').click(function() {
+	currentQ -= 2;
+	load_next_question(); 
+});
+$('.button.nextQ').click(function() {
+	load_next_question(); 
+});
+
+function load_next_question() {
+	if (currentQ < 0){
+		currentQ = 0;
+	}
+
+	if (currentQ == questions.length) {
+		console.log("out of questions do something")
+		if (questions[0] == firstQuestions[0]){
+			swal({
+				title: "Tutorial Complete!",
+				text: "Congratuations on completing the First Set tutorial!",
+				type: "success",
+				html:true,
+				confirmButtonText: "Continue to Follow Set Tutorial"
+			},
+			function() {
+				switchSection("follow");
+			});
+		} else if (questions[0] == followQuestions[0]){
+			swal({
+				title: "Tutorial Complete!",
+				text: "Congratuations on completing the Follow Set tutorial!",
+				type: "success",
+				html:true,
+				confirmButtonText: "Continue to Parse Table Tutorial"
+			}, 
+			function() {
+				switchSection("parse");
+			});
+		} else if (questions[0] == parseQuestions[0]){
+			swal({
+				title: "Tutorial Complete!",
+				text: "Congratuations on completing the Parse Table tutorial!",
+				type: "success",
+				html:true,
+				confirmButtonText: "Start Practicing"
+			},
+			function() {
+				window.location.href = '/practice';
+			});
+		}
+		return
+	}
+
+	if (currentQ == 0) {
+		$('.button.prevQ').hide();
+	} else {
+		$('.button.prevQ').show();
+	}
+
+	curQ = questions[currentQ];
+	// console.log(currentQ);
+	currentQ++;
+	
+	// console.log(curQ);
+
+	if (curQ.grammar)
+		$('#grammar').html(grammar_template(curQ.grammar))
+
+	if (curQ.type == "checkbox") {
+		$('#questions-container').html(question_template(curQ));
+		$('#active').fadeIn({duration:800});
+
+		// submit handler:
+		$('#question-input').on('submit', function() {
+			var answer = []
+			$("input[name=question-check]:checked").each(function() {
+				answer.push($(this).val());
+			});
+			// answer is right so:
+			if (answer.join(',') === curQ.answer) {
+				$('#question-input').remove()
+				$('#active').removeAttr('id')
+				load_next_question();
+			} else {
+				$('#question-input > .feedback').html("<p>Incorrect answer</p>")
+			}
+
+		});
+
+	} else {
+		console.log("non-checkbox questions not implemented yet")
+	}
+}
