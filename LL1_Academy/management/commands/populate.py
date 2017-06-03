91287e27595ca5b9291a0ac95ed2db1ad54b8d86
Vincent Siu <vince.siu4@gmail.com>
2017-06-03 20:45:21 +0100

    Upgrade functionality of populate command
    Added the "--reset" flag which will reset the database automatically
    when populating. Also, added two statements which showed how many
    grammar objects exist before and after in the db.

--- LL1_Academy/management/commands/populate.py
+++ LL1_Academy/management/commands/populate.py
@@ -1,15 +1,28 @@
 from django.core.management.base import BaseCommand, CommandError
 from LL1_Academy.tools import MassGrammarGenerator
+from LL1_Academy.models import Grammar
 import os
 import time
+from django.core.management import call_command
+
 
 class Command(BaseCommand):
 	help = 'This command will add grammars to the database, starting from <num> grammars and gradually filtering them down to a valid set (which may be much less than <num>)'
 
 	def add_arguments(self, parser):
 		parser.add_argument('num', type=int)
+		parser.add_argument('--reset',
+				action='store_true',
+				dest='reset_db',
+				default=False,
+				help='Clear database and repopulate it',
+				)
 	
 	def handle(self, *args, **options):
+		if (options['reset_db']):
+			call_command('cleardatabase')
+
+		print("Grammar objects initially in database: {}".format(Grammar.objects.count()))
 		#Number of randomly generated grammars
 		num = options['num']
 
@@ -26,3 +39,4 @@ class Command(BaseCommand):
 			mg = MassGrammarGenerator.MassGrammarGenerator(n)
 			mg.run(num,nonTerminals[:n],terminals)
 			print("{}Variables: {} seconds---".format(n,(time.time() - start_time)))
+		print("Grammar objects finally in database: {}".format(Grammar.objects.count()))
