47554445978a0660ce4dfd0048639631ad801baf
eliseyuen <skate4fun88@gmail.com>
2017-06-04 10:17:05 -0700

    Add more first set tutorial questions

--- cs130_LL1/static/js/tutorial-questions.js
+++ cs130_LL1/static/js/tutorial-questions.js
@@ -1,4 +1,4 @@
-var q1_grammar = {
+var first1_grammar = {
 	grammar: [
 	{
 		nt: 'A',
@@ -7,7 +7,7 @@ var q1_grammar = {
 }
 
 
-var q2_grammar = {
+var first2_grammar = {
 	grammar: [
     {
 		nt: 'A',
@@ -15,7 +15,7 @@ var q2_grammar = {
 	}]
 }
 
-var q3_grammar = {
+var first3_grammar = {
 	grammar: [
 	{
 		nt: 'A',
@@ -27,7 +27,7 @@ var q3_grammar = {
 	}]
 }
 
-var q4_grammar = {
+var first4_grammar = {
 	grammar: [
 	{
 		nt: 'A',
@@ -43,16 +43,85 @@ var q4_grammar = {
 	}]
 }
 
-var firstQuestions = [
+var first5_grammar = {
+	grammar: [
 	{
-		type: "text",
-		text: [
-			"example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing ",
-			"example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing"
-		],
+		nt: 'A',
+		productions: ['Bz', 'xC']
+    },
+    {
+		nt: 'B',
+		productions: ['Ax','yxx', 'C']
 	},
 	{
-		grammar: q1_grammar,
+		nt: 'C',
+		productions: ['z']
+	}]
+}
+
+var first6_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['xy', 'ε']
+    }]
+}
+
+var first7_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['Bx', 'z']
+    },
+	{
+		nt: 'B',
+		productions: ['ε']
+    }]
+}
+
+var first8_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['Bx', 'z']
+    },
+	{
+		nt: 'B',
+		productions: ['y','ε']
+    }]
+}
+
+var first9_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['B', 'x']
+    },
+	{
+		nt: 'B',
+		productions: ['ε']
+    }]
+}
+
+var first10_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['BC', 'Bz']
+    },
+	{
+		nt: 'B',
+		productions: ['y','ε']
+    },
+	{
+		nt: 'C',
+		productions: ['x','ε']
+    }]
+}
+
+var firstQuestions = [
+	{
+		grammar: first1_grammar,
 		helptext: "To calculate the First Set of A, we need to calculate the First Set for the production “xy”. For this case, we just find first terminal symbol of the production. What is the first terminal symbol of “xy”?",
 		question: "What is the first set of A?",
 		answer: "x",
@@ -60,7 +129,7 @@ var firstQuestions = [
 		terminals: ['x','y'],
 	},
 	{
-		grammar: q2_grammar,
+		grammar: first2_grammar,
 		helptext: "When there are multiple productions, we calculate the First Set of each production individually, and add each of them to the First Set. The First Set of A are the First Sets of “xy” and “yz” combined.",
 		question: "What is the first set of A?",
 		answer: "x,y",
@@ -68,7 +137,7 @@ var firstQuestions = [
 		terminals: ['x','y','z'],
 	},
 	{
-		grammar: q3_grammar,
+		grammar: first3_grammar,
 		helptext: "When there is a nonterminal as the first symbol of a production, then the First Set of that symbol is the First Set of that production. In this case, the First Set of A is “z”, and the First Set of B. Hint: It would be good to calculate First Set of B first, and then substitute it into the First Set of A.",
 		question: "What is the first set of B?",
 		answer: "y",
@@ -76,7 +145,7 @@ var firstQuestions = [
 		terminals: ['x','y','z'],
 	},
 	{
-		grammar: q3_grammar,
+		grammar: first3_grammar,
 		helptext: "When there is a nonterminal as the first symbol of a production, then the First Set of that symbol is the First Set of that production. In this case, the First Set of A is “z”, and the First Set of B. Hint: It would be good to calculate First Set of B first, and then substitute it into the First Set of A.",
 		question: "What is the first set of A?",
 		answer: "y,z",
@@ -84,35 +153,162 @@ var firstQuestions = [
 		terminals: ['x','y','z'],
 	},
 	{
-		grammar: q4_grammar,
-		helptext: "This is a challenge problem",
+		grammar: first4_grammar,
+		helptext: "Challenge Problem #1",
 		question: "What is the first set of A?",
 		answer: "x,y,z",
 		type: "checkbox",
 		terminals: ['x','y','z'],
 	},
 	{
-		grammar: q4_grammar,
-		helptext: "This is a challenge problem",
+		grammar: first4_grammar,
+		helptext: "Challenge Problem #1",
 		question: "What is the first set of B?",
 		answer: "x,z",
 		type: "checkbox",
 		terminals: ['x','y','z'],
 	},
 	{
-		grammar: q4_grammar,
-		helptext: "This is a challenge problem",
+		grammar: first4_grammar,
+		helptext: "Challenge Problem #1",
 		question: "What is the first set of C?",
 		answer: "x",
 		type: "checkbox",
 		terminals: ['x','y','z'],
+	},
+	{
+		grammar: first5_grammar,
+		helptext: "Challenge Problem #2",
+		question: "What is the first set of A?",
+		answer: "x,y,z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: first5_grammar,
+		helptext: "Challenge Problem #2",
+		question: "What is the first set of B?",
+		answer: "x,y,z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: first5_grammar,
+		helptext: "Challenge Problem #2",
+		question: "What is the first set of C?",
+		answer: "z",
+		type: "checkbox",
+		terminals: ['x','y','z'],
+	},
+	{
+		grammar: first6_grammar,
+		helptext: "Here, we introduce the concept of ε, which is can be considered a “null” string. In other words, A’s ε production converts the Nonterminal symbol to nothing. If there exists an epsilon production, we add ε to the First Set of A, and A is now considered a “nullable” nonterminal.",
+		question: "What is the first set of A?",
+		answer: "x,ε",
+		type: "checkbox",
+		terminals: ['x','y','ε'],
+	},
+	{
+		grammar: first7_grammar,
+		helptext: "UNFINISHED",
+		question: "What is the first set of A?",
+		answer: "x,z",
+		type: "checkbox",
+		terminals: ['x','z','ε'],
+	},
+	{
+		grammar: first7_grammar,
+		helptext: "UNFINISHED",
+		question: "What is the first set of B?",
+		answer: "ε",
+		type: "checkbox",
+		terminals: ['x','z','ε'],
+	},
+	{
+		grammar: first8_grammar,
+		helptext: "When we look at the first production of A, we see that B is the first symbol, so we want to add the First Set of B to the First Set of A. However, this is incorrect! If we see that B has a ε in its First Set and is nullable, we will add everything EXCEPT the ε from B to First Set of A, and then look at the first set of the rest of the production. So, in this case, we would see that B is nullable, and add only “y” to the First Set of A, and then look at the First Set of everything after B, which is just “x”.",
+		question: "What is the first set of A?",
+		answer: "x,z",
+		type: "checkbox",
+		terminals: ['x','y','z','ε'],
+	},
+	{
+		grammar: first8_grammar,
+		helptext: "When we look at the first production of A, we see that B is the first symbol, so we want to add the First Set of B to the First Set of A. However, this is incorrect! If we see that B has a ε in its First Set and is nullable, we will add everything EXCEPT the ε from B to First Set of A, and then look at the first set of the rest of the production. So, in this case, we would see that B is nullable, and add only “y” to the First Set of A, and then look at the First Set of everything after B, which is just “x”.",
+		question: "What is the first set of B?",
+		answer: "y,ε",
+		type: "checkbox",
+		terminals: ['x','y','z','ε'],
+	},
+	{
+		grammar: first9_grammar,
+		helptext: "For the first production of A, we see that it only consists of B. Since B is nullable and can go to null string, we now know that A is also nullable, and add ε to the First Set of A.",
+		question: "What is the first set of A?",
+		answer: "x,ε",
+		type: "checkbox",
+		terminals: ['x','ε'],
+	},
+	{
+		grammar: first9_grammar,
+		helptext: "For the first production of A, we see that it only consists of B. Since B is nullable and can go to null string, we now know that A is also nullable, and add ε to the First Set of A.",
+		question: "What is the first set of B?",
+		answer: "ε",
+		type: "checkbox",
+		terminals: ['x','ε'],
+	},
+	{
+		grammar: first10_grammar,
+		helptext: "Challenge Problem #3",
+		question: "What is the first set of A?",
+		answer: "x,y,z,ε",
+		type: "checkbox",
+		terminals: ['x','y','z','ε'],
+	},
+	{
+		grammar: first10_grammar,
+		helptext: "Challenge Problem #3",
+		question: "What is the first set of B?",
+		answer: "y,ε",
+		type: "checkbox",
+		terminals: ['x','y','z','ε'],
+	},
+	{
+		grammar: first10_grammar,
+		helptext: "Challenge Problem #3",
+		question: "What is the first set of C?",
+		answer: "x,ε",
+		type: "checkbox",
+		terminals: ['x','y','z','ε'],
+	},
+	{
+		type: "text",
+		text: [
+			"example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing ",
+			"example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing example of a paragraph in a full column text explaine rthing"
+		],
 	}
 ]
+
+var follow1_grammar = {
+	grammar: [
+	{
+		nt: 'A',
+		productions: ['BC', 'zC']
+    },
+    {
+		nt: 'B',
+		productions: ['Cx','yBy']
+	},
+	{
+		nt: 'C',
+		productions: ['xz']
+	}]
+}
 
 var followQuestions = [
 	// filler code
 	{
-		grammar: q3_grammar,
+		grammar: first3_grammar,
 		helptext: "THIS IS FILLER CODE FOR FOLLOW QUESTIONS",
 		question: "THIS IS FILLER CODE FOR FOLLOW QUESTIONS",
 		answer: "y,z",
@@ -124,7 +320,7 @@ var followQuestions = [
 var parseQuestions = [
 	// filler code
 	{
-		grammar: q3_grammar,
+		grammar: first3_grammar,
 		helptext: "THIS IS FILLER CODE FOR PARSE TABLE QUESTIONS",
 		question: "THIS IS FILLER CODE FOR PARSE TABLE QUESTIONS",
 		answer: "y",
