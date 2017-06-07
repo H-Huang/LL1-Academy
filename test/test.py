30ca66506621faff275c78c505ed876e8432df97
Vincent Siu <vince.siu4@gmail.com>
2017-06-07 08:44:30 +0100

    Add UI tests

--- test/test.py
+++ test/test.py
@@ -30,11 +30,50 @@ class UITest(unittest.TestCase):
         driver = self.driver
         self.assertEqual('LL(1) Academy - Homepage', driver.title)
 
-    def test_tutorial(self):
-        pass
-
     def test_practice(self):
-        pass
+        driver = self.driver
+        driver.implicitly_wait(10)
+        elem = driver.find_element_by_partial_link_text('begin')
+        elem.click()
+        WebDriverWait(driver, 10).until(
+                EC.title_contains('Tutorial')
+                )
+        elem = driver.find_element_by_partial_link_text('practicing')
+        elem.click()
+        time.sleep(0.5)
+        elem = driver.find_element_by_id('giveup')
+        elem.click()
+        time.sleep(0.5)
+        elem = driver.find_element_by_id('giveup')
+        elem.click()
+        time.sleep(0.5)
+        elem = driver.find_element_by_id('giveup')
+        elem.click()
+        
+
+    def test_tutorial(self):
+        driver = self.driver
+        elem = driver.find_element_by_partial_link_text('begin')
+        elem.click()
+        WebDriverWait(driver, 10).until(
+                EC.title_contains('Tutorial')
+                )
+       
+        elem = driver.find_element_by_id('firstTutorial')
+        elem.click()
+        time.sleep(0.5)
+        elem = driver.find_element_by_class_name('question-help')
+        self.assertIn('First Set',elem.text)
+        elem = driver.find_element_by_id('followTutorial')
+        elem.click()
+        time.sleep(0.5)
+        elem = driver.find_element_by_class_name('question-help')
+        self.assertIn('Follow Set',elem.text)
+        elem = driver.find_element_by_id('parseTutorial')
+        elem.click()
+        time.sleep(0.5)
+        elem = driver.find_element_by_class_name('question-help')
+        self.assertIn('First(xA)',elem.text)
 
     def test_about(self):
         driver = self.driver
@@ -43,6 +82,7 @@ class UITest(unittest.TestCase):
         WebDriverWait(driver, 10).until(
                 EC.title_contains('About')
                 )
+        self.assertEqual('LL(1) Academy - About', driver.title)
         
     def test_github(self):
         driver = self.driver
@@ -52,11 +92,28 @@ class UITest(unittest.TestCase):
                 EC.title_contains('LL(1) Academy')
                 )
         self.assertIn('LL(1) Academy - Homepage', driver.title)
+        time.sleep(1)
+
+    def test_bug_report(self):
+        driver = self.driver
+        elem = driver.find_element_by_partial_link_text('begin')
+        elem.click()
+        WebDriverWait(driver, 10).until(
+                EC.title_contains('Tutorial')
+                )
+        self.assertEqual('LL(1) Academy - Tutorial', driver.title)
+        elem = driver.find_element_by_partial_link_text('Report a Bug')
+        elem.click()
+        time.sleep(2)
+        self.assertEqual('LL(1) Academy Bug Report', driver.title)
+
+
 
     def tearDown(self):
+        time.sleep(1)
         if self.driver is not None:
             self.driver.quit()
 
 if __name__ == '__main__':
-    time.sleep(10)
+    time.sleep(5)
     unittest.main()
