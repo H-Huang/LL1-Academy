234de793846a4731dae4a5b642a6e16f864fff6d
Vincent Siu <vince.siu4@gmail.com>
2017-06-07 04:00:24 +0100

    Implement retry library
    Hooray for exponential backoff and python decorators! However, I think I
    found a bug where if you give an invalid URL to the executor, then it
    will hang forever, no exceptions passed.

--- test/test.py
+++ test/test.py
@@ -1,25 +1,24 @@
 import argparse
+from retrying import retry
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+@retry
+def connect(executor, capabilities):
+    print("hi")
+    driver = webdriver.Remote(command_executor=executor, desired_capabilities=capabilities)
+    return driver
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run UI tests on LL1 Academy website")
     parser.add_argument('host_selenium', metavar='host_selenium', help='hostname of selenium webdriver to connect to')
     parser.add_argument('host_website', metavar='host_website', help='hostname of website to test against')
 
     args = parser.parse_args()
-
-    print(args.host_selenium)
-    print(args.host_website)
-
     
-#    while True:
-#        pass
     capabilities = DesiredCapabilities.CHROME
     executor = "http://" + args.host_selenium + ":4444/wd/hub"
-    print(executor)
     
-    driver = webdriver.Remote(command_executor=executor, desired_capabilities=DesiredCapabilities.CHROME)
+    driver = connect(executor, capabilities)
     driver.get(args.host_website)
     print (driver.title)
 
