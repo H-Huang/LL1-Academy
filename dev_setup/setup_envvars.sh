3115a083eadef6e5445e4e7bce7d175a26cfbbe3
Vincent Siu <vince.siu4@gmail.com>
2017-06-10 05:58:50 +0100

    Add setup_envvars.sh script for env vars

---
+++ dev_setup/setup_envvars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export POSTGRES_HOST=db
+export POSTGRES_NAME=postgres
+export POSTGRES_PORT=5432
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=
+export DJANGO_SECRETKEY='sc**kkwq+#zb)s-0duj-k-j^92l-cj6x^7*-*eyba#6+obwftf'
+export DJANGO_PROD=0
